7523f0a17c66db061cd13be76495f1277f76ce84
duxor <dullle@live.com>
2015-04-09 21:39:56 +0200

    3.2 MAPA

--- app/Objekat.php
+++ app/Objekat.php
@@ -5,6 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Objekat extends Model{
-    protected $table = 'objetak';
+    protected $table = 'objekat';
     protected $fillable = ['naziv','created_at', 'updated_at','x','y','adresa','aktivan','vrsta_objekta_id','grad_id','nalog_id'];
 }
