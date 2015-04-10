755facd80cf91aedf52e35297b5894695823ce36
duxor <dullle@live.com>
2015-04-10 20:31:44 +0200

    А 3.7

---
+++ app/Http/Controllers/Log.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+use App\Http\Requests;
+use App\Security;
+
+class Log extends Controller {
+	public function getLogin(){
+		if(Security::autentifikacijaTest()) return Security::rediectToLogin();
+		return view('stranice.administracija.login');
+	}
+	public function postLogin(){
+		return Security::login(Input::get('username'),Input::get('password'));
+	}
+	public function getLogout(){
+		return Security::logout();
+	}
+}
