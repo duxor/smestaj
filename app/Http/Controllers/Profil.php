08c7c2703fab06ed30dd67f8556736baa0863bb6
saxoman <jovicsasajovic@gmail.com>
2015-04-11 20:27:28 +0200

    3.4
    des

--- app/Http/Controllers/Profil.php
+++ app/Http/Controllers/Profil.php
@@ -8,6 +8,7 @@ use App\Security;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 
 use Illuminate\Http\Request;
@@ -31,24 +32,30 @@ class Profil extends Controller {
 	}
 
     public function postLogin(){
+    		$data=Input::all();
+			$rules = array(
+	        'username'	=> 'Required|Between:5,12',
+	        'password'  =>'Required|AlphaNum|Between:4,8|',
+			);
+
+			$v=Validator::make($data,$rules);
+
+			if($v->fails())
+			{
+				return Redirect::to('/profil/login')->withErrors($v->errors());
+			}
 		return Security::login(Input::get('username'),Input::get('password'));
 	}
 
-	public function getProfil(){
-	}
 	public function getEditNalog(){
-
 		if(Security::autentifikacijaTest())
 		{
 			$ids=Session::get('id');
 			$korisnik=Korisnici::where('id', '=', $ids)->get(['id','ime','prezime','email','username'])->first()->toArray();
 			return view('korisnik.profil.edit',compact('korisnik'));
 		}return view('korisnik.prijava.index');
-		
-		
 	}
 	public function postEditNalog(){
-
 		$korisnik= Korisnici::firstOrNew(['id'=>Input::get('id')],['id','prezime','ime' ,'username','email']);  
 		$korisnik->prezime=Input::get('prezime');
 		$korisnik->ime=Input::get('ime');
@@ -58,16 +65,26 @@ class Profil extends Controller {
 		$ids=Session::get('id');
 		$korisnik=Korisnici::where('id', '=', $ids)->get(['id','ime','prezime','email','username'])->first()->toArray();
 		return view('korisnik.profil.index',compact('korisnik'));
-		
 	}
-		public function postRegistracija(){
-			Security::registracija(Input::get('username2'),Input::get('email2'),Input::get('password2'),Input::get('prezime2'), Input::get('ime2'));
+	public function postRegistracija(){
 
-		
-		
+			$un=Input::get('username2');
+			$email=Input::get('email2');
+
+			$data=['username'=>$un,'email'=>$email];
+			$rules = array(
+	        'username'	=> 'Required|Between:5,12|Unique:korisnici',
+	        'email'     => 'Required|Between:3,64|Email|Unique:korisnici',
+			);
+
+			$v=Validator::make($data,$rules);
+
+			if($v->fails())
+			{
+				return Redirect::to('/profil/login')->withErrors($v->errors());
+			}
+        	Security::registracija(Input::get('username2'),Input::get('email2'),Input::get('password2'),Input::get('prezime2'), Input::get('ime2'));
+                		
+
 	}
-
-
-
-
 }
