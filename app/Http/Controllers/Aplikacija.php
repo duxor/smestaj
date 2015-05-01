f4399b85455ce6e41c9821decce2bed38ebbc895
duxor <dullle@live.com>
2015-05-01 20:46:05 +0200

    А 5.9.3

--- app/Http/Controllers/Aplikacija.php
+++ app/Http/Controllers/Aplikacija.php
@@ -17,16 +17,16 @@ class Aplikacija extends Controller {
 			return view('aplikacija.teme-osnove.osnovna.index',compact('podaci'));
 		}
 		//#######################
-		$nalog=Nalog::join('tema','tema.id','=','nalog.tema_id')->where('nalog.slug',$slug)->where('nalog.aktivan',1)->get(['nalog.id','tema_id','nalog.naziv','nalog.slug as nalog_slug','tema.slug as tema_slug'])->first()->toArray();
+		$nalog=Nalog::join('tema','tema.id','=','nalog.tema_id')->where('nalog.slug',$slug)->where('nalog.aktivan',1)->get(['nalog.id','tema_id','nalog.naziv','nalog.slug as nalog_slug','tema.slug as tema_slug'])->first();
 		if($nalog){
+			$nalog=$nalog->toArray();
 			$podaci=Templejt::join('sadrzaji','sadrzaji.templejt_id','=','templejt.id')->where('nalog_id',$nalog['id'])->where('tema_id',$nalog['tema_id'])->where('vrsta_sadrzaja_id','<>',6)->orderBy('redoslijed')->get(['slug','naziv','sadrzaj','vrsta_sadrzaja_id','icon'])->toArray();
 			$podaci['pocetna']=true;
 			$podaci['pozadine']=Templejt::join('sadrzaji','sadrzaji.templejt_id','=','templejt.id')->where('nalog_id','=',1)->where('tema_id','=',1)->where('vrsta_sadrzaja_id','=',6)->orderBy('redoslijed')->get(['sadrzaj'])->toArray();
 			$podaci['grad']=Grad::join('objekat','objekat.grad_id','=','grad.id')->where('objekat.nalog_id',$nalog['id'])->orderBy('grad.id')->get(['grad.id','grad.naziv'])->lists('naziv','id');
 			$podaci['app']=$nalog['id'];
-			//dd($podaci);
 			return view("aplikacija.teme.{$nalog['tema_slug']}.index",compact('podaci'));
-		}else return 'Greska!!!';
+		}else return 'Aplikacija nije aktivna!';
 		//return "Dobor došli na <b>{$nalog['naziv']}</b> platformu. SLUG:{$nalog['nalog_slug']},TEMA:{$nalog['tema_id']}";
 	}
 	public function getObjekat($slug){
