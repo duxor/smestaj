986fd7c35aef965bcbe1818fa30aac1de4d21e5b
duxor <dullle@live.com>
2015-05-14 23:44:44 +0200

    A 7.5.5

--- app/Http/Controllers/MailboxC.php
+++ app/Http/Controllers/MailboxC.php
@@ -1,12 +1,14 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Requests;
-
+use App\Mailbox;
+use App\Security;
+use Illuminate\Support\Facades\Session;
 class MailboxC extends Controller {
 	public function mailbox($slug,$akcija){
 		$podaci['prava']=$slug;
 		$podaci['akcija']=$akcija;
-		return view('mailbox.index',compact('podaci'));
+		return Security::autentifikacija('mailbox.index',compact('podaci'));
 	}
 	public function getIndex($pravaSlug){
 		return redirect($pravaSlug.'/mailbox/inbox');
@@ -14,9 +16,14 @@ class MailboxC extends Controller {
 	public function getKreiraj($pravaSlug){
 		return $this->mailbox($pravaSlug,'nova');
 	}
+//INBOX
 	public function getInbox($pravaSlug){
 		return $this->mailbox($pravaSlug,'inbox');
 	}
+	public function postUcitajInbox(){
+		return json_encode(Mailbox::where('korisnici_id',Session::get('id'))->orderby('created_at','DESC')->get(['od_email','naslov','poruka','procitano','created_at'])->toArray());
+	}
+
 	public function getPoslate($pravaSlug){
 		return $this->mailbox($pravaSlug,'poslate');
 	}
--- resources/views/mailbox/index.blade.php
+++ resources/views/mailbox/index.blade.php
@@ -3,11 +3,24 @@
     <div class="col-sm-3">
         <ul class="nav nav-pills nav-stacked">
             <li role="presentation" @if($podaci['akcija']=='nova') class="active" @endif><a href="#">Kreiraj poruku</a></li>
-            <li role="presentation" @if($podaci['akcija']=='inbox') class="active" @endif><a href="#">Inbox</a></li>
+            <li role="presentation" @if($podaci['akcija']=='inbox') class="active" @endif><a href="#"onclick="getInbox()">Inbox</a></li>
             <li role="presentation" @if($podaci['akcija']=='poslate') class="active" @endif><a href="#">Poslate</a></li>
         </ul>
     </div>
-    <div class="col-sm-9">
+    <div id="work-area" class="col-sm-9">
 
     </div>
+    <script>
+        function getInbox(){
+            $.post('/{{$podaci['prava']}}/mailbox/ucitaj-inbox',{
+                _token:'{{csrf_token()}}'
+            },function(data){
+                var inbox='<table class="table table-hover">';
+                var podaci=JSON.parse(data);
+                for(var i=0;i<podaci.length;i++)
+                    inbox+='<tr><td>'+podaci[i].od_email+'</td><td>'+podaci[i].naslov+'</td><td>'+podaci[i].poruka+'</td><td>'+podaci[i].created_at+'</td></tr>';
+                $('#work-area').html(inbox+'</table>');
+            });
+        }
+    </script>
 @endsection
