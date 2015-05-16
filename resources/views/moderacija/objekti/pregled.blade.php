a03e1e5f90e4a1bfa32fddb2b97cb37a71d78899
duxor <dullle@live.com>
2015-05-16 15:47:54 +0200

    A 8.2.8

--- resources/views/moderacija/objekti/pregled.blade.php
+++ resources/views/moderacija/objekti/pregled.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="col-sm-4">
-	{!! Form::open(['url'=>'/moderator/pregledobjekata', 'name' => "forma"]) !!}
+	{!! Form::open(['url'=>'/moderacija/pregledobjekata', 'name'=>"forma",'method'=>'post']) !!}
 		{!! Form::select('nalog', array('-1' => 'Izaberite Aplikaciju ...') + $nalog, null, array('name' => 'nalog', 'class'=>'form-control',"onchange" => "document.forma.submit();") )!!}
 	{!! Form::close() !!}
 </div>
--- resources/views/moderacija/objekti/smestaj.blade.php
+++ resources/views/moderacija/objekti/smestaj.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="col-sm-4">
-	{!! Form::open(['url'=>'/moderator/pregled-smestaja', 'name' => "forma"]) !!}
+	{!! Form::open(['url'=>'/moderacija/pregled-smestaja', 'name' => "forma"]) !!}
 		{!! Form::select('nalog', array('-1' => 'Izaberite Aplikaciju ...') + $nalog, null, ['name' => 'nalog', 'class'=>'form-control',"onchange" => "document.forma.submit();"] )!!}
 	{!! Form::close() !!}
 </div>
