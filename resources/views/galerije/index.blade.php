564ac3d91ffd539efa7063c176c288d748951d0c
duxor <dullle@live.com>
2015-05-16 20:19:52 +0200

    A 8.3.2

--- resources/views/galerije/index.blade.php
+++ resources/views/galerije/index.blade.php
@@ -2,23 +2,24 @@
 @section('body')
     @if($podaci['galerije'])
         <div class="list-group col-sm-4">
-            <p style="text-align: right"><button class="btn btn-info editMod" style="margin-right: 5px"><i class="glyphicon glyphicon-pencil"></i></button></p>
+            <p style="text-align: right">
+                <button class="btn btn-default sakrijListu"><i class="glyphicon glyphicon-circle-arrow-left strelica"></i></button>
+                <button class="btn btn-info editMod" style="margin:0 5px"><i class="glyphicon glyphicon-pencil"></i></button>
+            </p>
             @foreach($podaci['galerije'] as $galerija)
                 <a style="cursor: pointer" class="list-group-item" data-slugapp="{{$galerija['slugApp']}}" data-link="galerije/{{Session::get('username')}}/aplikacije/{{$galerija['slugApp']}}/{{$galerija['slugTeme']}}/{{$galerija['sadrzaj']}}">
                     <button class="btn btn-success _upload" data-toggle="modal" data-target="#dodajFoto" style="position: absolute;right: 5px;top: 5px"><i class="glyphicon glyphicon-cloud-upload"></i></button>
                     <h2 style="text-align: center">{{$galerija['naziv']}}</h2>
                     <p class="list-group-item-text">
-                        <dl class="dl-horizontal">
-                            <dt>Aplikacija</dt>
-                            <dd>{{$galerija['nazivApp']}}</dd>
-                            <dt>Tema</dt>
-                            <dd>{{$galerija['nazivTeme']}}</dd>
-                        </dl>
+                    <table>
+                        <tr><td>Aplikacija </td><td><b>{{$galerija['nazivApp']}}</b></td></tr>
+                        <tr><td>Tema </td><td><b>{{$galerija['nazivApp']}}</b></td></tr>
+                    </table>
                     </p>
                 </a>
             @endforeach
         </div>
-        <div class="col-sm-8">
+        <div class="col-sm-8 work-area">
             <div id="poruka" style="display: none"></div>
             <div id="wait" style="display:none"><center><i class='icon-spin6 animate-spin' style="font-size: 350%"></i></center></div>
             <div id="foto"></div>
@@ -26,6 +27,18 @@
             <i class='icon-spin6 animate-spin' style="color: rgba(0,0,0,0)"></i>
         </div>
         <script>
+            var sirina='50px';
+            $('.sakrijListu').click(function(){$('.work-area').hide();
+                $('.list-group').animate({width:sirina},350);
+                $('.editMod').toggle();
+                $('.list-group-item').toggle();
+                $('.strelica').toggleClass('glyphicon-circle-arrow-left');
+                $('.strelica').toggleClass('glyphicon-circle-arrow-right');
+                $(this).show();
+                $('.work-area').animate({width:sirina=='50px'?'100%':'70%'},350);
+                $('.work-area').fadeIn();
+                sirina=sirina=='50px'?'30%':'50px';
+            });
             var editMod=false, slugApp=false, serverUrl='{{url('/')}}}';
             $('.editMod').click(function(){editMod=!editMod;$('.prikaz').popover('hide')});
             function slikaOption(){if(editMod)$('.prikaz').popover('toggle')}
@@ -95,7 +108,7 @@
                     {!!Form::hidden('folder',null,['id'=>'folder'])!!}
                 </div>
                 <div class="modal-footer">
-                    <a href="#" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-ok"></span> Završeno dodavanje</a>
+                    <a href="#" class="btn btn-lg btn-primary" data-dismiss="modal"><span class="glyphicon glyphicon-ok"></span> Završeno dodavanje</a>
                 </div>
             </div>
         </div>
