39d27fa2cda01bf7989f651d87a21e5c3d298aaa
saxoman <jovicsasajovic@gmail.com>
2015-05-17 18:26:59 +0200

    gosti

--- resources/views/moderacija/rezervacija/gosti.blade.php
+++ resources/views/moderacija/rezervacija/gosti.blade.php
@@ -4,25 +4,25 @@
     <div class="container">
     @foreach($korisnici as $kor)
         <div class="row">
-        <div class="panel panel-default">
+        <div style="border:1px solid #E6E6E6; padding-top:20px;" >
             <div class="col-sm-2">
-                    <img src="/galerije/korisnik/osnovne/profilna.jpg" class="img img-circle" style="height:80px; width:80px;">
-                  <br clear="all"><br clear="all">
+                    <img src="/galerije/korisnik/osnovne/profilna.jpg" class="img img-circle" style="height:60px; width:60px;">
+                  <br clear="all">
                 <p>
                     <a href="/moderacija/mailbox/kreiraj/{{$kor['username']}}" class="btn btn-lg btn-default"><i class="glyphicon glyphicon-envelope"></i> Pošalji poruku</a>
                 </p>
             </div>
             <div class="col-sm-7">       
-                <table style="border-left:5px solid #74ABFB;" class="table table-condensed">
-                    <tr ><td>Prezime i ime:</td><td>{{$kor['pr']}} {{$kor['ime_korisnika']}}</td></tr>
-                    <tr><td>Username:</td><td> {{$kor['username']}}</td></tr>
-                    <tr><td>Ocena:</td><td>{{$kor['ocena']}} </td></tr>
+                <table  style="border-left:5px solid #74ABFB; "class="moja-tabela">
+                    <tr ><td class="nDn"> Prezime i ime:</td><td>{{$kor['pr']}} {{$kor['ime_korisnika']}}</td></tr>
+                    <tr><td class="nDn">Username:</td><td> {{$kor['username']}}</td></tr>
+                    <tr><td class="nDn">Ocena:</td><td>{{$kor['ocena']}} </td></tr>
                 </table>
             </div>
              <div class="col-sm-3">
-                <table class="table table-condensed">
-                    <tr><td>Utisci:</td><td></td></tr>
-                    <tr><td>{!!$kor['utisci']!!}</td></tr>
+                <table class="moja-tabela">
+                    <tr><td class="nDn">Utisci:</td><td></td></tr>
+                    <tr><td class="nDn">{!!$kor['utisci']!!}</td></tr>
                 </table>
             </div>
             <br clear="all">
