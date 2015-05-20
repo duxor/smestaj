806fa41315672c8f8e31f0e32a17fae5a04c4907
duxor <dullle@live.com>
2015-05-20 22:43:53 +0200

    A 8.8.2
    KOMENTARI

--- resources/views/moderacija/aplikacija/komentari.blade.php
+++ resources/views/moderacija/aplikacija/komentari.blade.php
@@ -29,22 +29,21 @@
 									</thead>
 									<tbody>
 									@foreach($komentari as $kom)
-										<tr>
-											<div id="forma-{{$kom['id']}}" class="form-horizontal">
-												{!!Form::hidden('id_komentara',$kom['id'])!!}
-												{!!Form::hidden('_token',csrf_token())!!}
-												<td style="text-align: center"><i @if($kom['aktivan']==1) class="glyphicon glyphicon-ok" @else class="glyphicon glyphicon-remove" @endif ></i></td>
+										<tr id="forma-{{$kom['id']}}">
+												<td style="text-align: center">{!!Form::hidden('id_komentara',$kom['id'])!!}{!!Form::hidden('_token',csrf_token())!!}<i @if($kom['aktivan']==1) class="glyphicon glyphicon-ok" @else class="glyphicon glyphicon-remove" @endif ></i></td>
 												<td>{{$kom['username']}}</td>
 												<td>{{$kom['slug']}}</td>
 												<td>{{$kom['komentar']}}</td>
 												<td>Smeštaj</td>
 												<td></td>
-												<td><p data-placement="top" data-toggle="tooltip" title="Ukloni komentar">{!! Form::button('<span class="glyphicon glyphicon-remove"></span>',['class'=>'btn btn-xs btn-danger','onclick'=>'Komunikacija.posalji("/moderacija/zabrani",\'forma-'.$kom['id'].'\',\'poruka\',\'vrti\',\'zabrani\')']) !!}</p></td>
-												<td><p data-placement="top" data-toggle="tooltip" title="Odobri komentar">{!! Form::button('<span class="glyphicon glyphicon-ok"></span>',['class'=>'btn btn-xs btn-success','onclick'=>'Komunikacija.posalji("/moderacija/odobri",\'forma-'.$kom['id'].'\',\'poruka\',\'vrti\',\'zabrani\')']) !!}</p></td>
+												<td><p data-placement="top" data-toggle="tooltip" title="Ukloni komentar">{!! Form::button('<span class="glyphicon glyphicon-remove"></span>',['class'=>'btn btn-xs btn-danger zabrani','onclick'=>'Komunikacija.posalji("/moderacija/zabrani",\'forma-'.$kom['id'].'\',\'poruka\',\'vrti\',\'zabrani\')']) !!}</p></td>
+												<td><p data-placement="top" data-toggle="tooltip" title="Odobri komentar">{!! Form::button('<span class="glyphicon glyphicon-ok"></span>',['class'=>'btn btn-xs btn-success odobri','onclick'=>'Komunikacija.posalji("/moderacija/odobri",\'forma-'.$kom['id'].'\',\'poruka\',\'vrti\',\'zabrani\')']) !!}</p></td>
 												<td><a data-placement="top" data-toggle="tooltip" title="Odgovori na komentar" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-envelope"></span></a></td>
-											</div>
 										</tr>
 									@endforeach
+									<script>
+										$('.odobri,.zabrani').click(function(){$('#'+$(this).closest('tr').attr('id')).fadeOut()});
+									</script>
 									</tbody>
 								</table>
 							</div>
